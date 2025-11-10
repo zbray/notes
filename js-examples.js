af5e7d10e24c532c8cdc194d608615bf55687acc
zachonemac <zachzebray@gmail.com>
2025-11-09 22:06:00 -0500

    fixes

--- js-examples.js
+++ js-examples.js
@@ -1,7 +1,6 @@
 // Examples of Functions
 // Write a function where an even number returns “Even” and an odd number returns “Odd”
 
-
 function even_or_odd(number) {
   if (number % 2 === 0) {
     return "Even";
@@ -9,19 +8,16 @@ function even_or_odd(number) {
     return "Odd";
   }
 
+  // Write a function that returns a string with all the vowels removed
 
-// Write a function that returns a string with all the vowels removed
+  function disemvowel(str) {
+    return str.replace(/[aeiou]/gi);
+  }
 
-function  disemvowel(str) {
-  Return str.replace(/[aeiou]/gi, ‘’);
-}
+  // Abbreviate a Two Word Name
 
-
-// Abbreviate a Two Word Name
-
-
-function abbrevName(name) {
-    const [firstName, lastName] = name.split(' ');
+  function abbrevName(name) {
+    const [firstName, lastName] = name.split(" ");
     return `${firstName[0].toUpperCase()}.${lastName[0].toUpperCase()}`;
-}
+  }
 }
